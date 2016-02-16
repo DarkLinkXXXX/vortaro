63feadbeb90a1a7c58e82043cd22e87dbe38c144
_@thomaslevine.com <_@thomaslevine.com>
2016-02-16 06:32:31 +0000

    fix typo
    FossilOrigin-Name: d899901d31557bac2254971a80022783af50b271

--- convert-to-dict.sh
+++ convert-to-dict.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-name="$(basename "$1" .txt)
+name=$(basename "$1" .txt)
 
 sed -e 's/\t/:/' -e 's/\t/ (/' -e 's/$/)/' "$1" |
-  dictfmt --utf8 --allchars -s "dict.cc $name" -j dict.cc-$name
+  dictfmt --utf8 --allchars -s "dict.cc $name" -j "dict.cc-$name"
