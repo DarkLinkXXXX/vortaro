ceeb299cbf028dc6de1a33f1212be9f9307cd2c6
tlevine <tlevine@noemail.net>
2017-12-10 23:15:13 +0000

    better
    FossilOrigin-Name: edc6ffc5b6076c27c04c882eb5243c5888a42b66

--- vortaro/db.py
+++ vortaro/db.py
@@ -23,7 +23,7 @@ from collections import defaultdict
 from . import transliterate
 
 LOG_INTERVAL = 10000
-N = 3 # Fragment size
+MAX_PHRASE_LENGTH = 12
 
 def history(data, search):
     with (data / 'history').open('a') as fp:
@@ -53,16 +53,10 @@ def _add_pair(con, from_lang, to_lang):
 def search(con, x):
     root = x.lower()
     if set(root).issubset(transliterate.full_alphabet):
-        tpl = 'fragment:%s'
-        keys = tuple(tpl % f for f in set(_search_fragments(root)))
-        if len(keys) == 1:
-            key = tuple(keys)[0]
-        else:
-            key = 'tmp:phrases'
-            con.zinterstore(key, keys, 'min')
-        for phrase, _ in con.zscan_iter(key):
-            if root in phrase.decode('utf-8'):
-                yield from map(pickle.loads, con.hvals(b'phrase:%s' % phrase))
+        for i in range(len(root), MAX_PHRASE_LENGTH+1):
+            for phrase in con.sscan_iter(b'lengths:%d' % i):
+                if root in phrase.decode('utf-8'):
+                    yield from map(pickle.loads, con.hvals(b'phrase:%s' % phrase))
 
 def index(con, formats, data, force=False):
     '''
@@ -84,43 +78,35 @@ def index(con, formats, data, force=False):
     fragments = defaultdict(list)
     for name, module in formats.items():
         directory = data / name
-        if not directory.is_dir():
-            continue
-        for file in directory.iterdir():
-            if not _get_out_of_date(con, file):
-                continue
-            files += 1
-            for line in module.read(file):
-                phrase = _restrict_chars(line['from_lang'], line.pop('search_phrase'))
-                for fragment in set(_index_fragments(phrase)):
-                    fragments[fragment].append(phrase)
-                xs = (
-                    line['from_lang'], line['from_word'],
-                    line['to_lang'], line['to_word'],
-                )
-                identifier = md5('\n'.join(xs).encode('utf-8')).hexdigest()
-                pairs.add((line['from_lang'], line['to_lang']))
-                con.hset('phrase:%s' % phrase, identifier, pickle.dumps(line))
+        if directory.is_dir():
+            for file in directory.iterdir():
+                if not _get_out_of_date(con, file):
+                    skip += 1
+                else:
+                    files += 1
+                    for line in module.read(file):
+                        phrase = _restrict_chars(line['from_lang'], line.pop('search_phrase'))
+                        xs = (
+                            line['from_lang'], line['from_word'],
+                            line['to_lang'], line['to_word'],
+                        )
+                        identifier = md5('\n'.join(xs).encode('utf-8')).hexdigest()
+                        pairs.add((line['from_lang'], line['to_lang']))
 
-                definitions += 1
-                if definitions % LOG_INTERVAL == 0:
-                    for fragment, phrases in fragments.items():
-                        def scored():
-                            for phrase in set(phrases):
-                                yield len(phrase) 
-                                yield phrase
-                        con.zadd('fragment:%s' % fragment, *scored())
-                    
-                    for pair in pairs:
-                        _add_pair(con, *pair)
-                    pairs.clear()
+                        con.sadd(b'lengths:%d' % len(phrase), phrase)
+                        con.hset('phrase:%s' % phrase, identifier, pickle.dumps(line))
 
-                    msg = '\rIndexed %d definitions from %d files (Skipped %d already-indexed files)'
-                    stderr.write(msg % (definitions, files, skip))
-                    
-            _set_up_to_date(con, file)
-        else:
-            skip += 1
+                        definitions += 1
+                        if definitions % LOG_INTERVAL == 0:
+                            for pair in pairs:
+                                _add_pair(con, *pair)
+                            pairs.clear()
+
+                            msg = '\rIndexed %d definitions from %d files' \
+                                  ' (Skipped %d already-indexed files)'
+                            stderr.write(msg % (definitions, files, skip))
+                            
+                _set_up_to_date(con, file)
 
 def _restrict_chars(lang, text):
     return getattr(transliterate, lang, transliterate.identity).to_roman(text).lower()
