fce8b39c43653cab629a3716429c590e6b54e748
tlevine <tlevine@noemail.net>
2018-12-16 09:48:16 +0000

    counts
    FossilOrigin-Name: 6e97e50fcef8dbbccbd6a333245abb32725166fc

--- vortaro/__init__.py
+++ vortaro/__init__.py
@@ -117,7 +117,6 @@ def search(text: Word, limit: int=ROWS-2, *, width: int=COLUMNS,
     ToLanguage = aliased(Language)
     FromLanguage = aliased(Language)
     q_joins = session.query(Dictionary) \
-        .join(PartOfSpeech, Dictionary.part_of_speech_id == PartOfSpeech.id) \
         .join(FromLanguage, Dictionary.from_lang_id == FromLanguage.id) \
         .join(ToLanguage,   Dictionary.to_lang_id   == ToLanguage.id)
     if from_langs:
@@ -126,19 +125,22 @@ def search(text: Word, limit: int=ROWS-2, *, width: int=COLUMNS,
         q_joins = q_joins.filter(ToLanguage.code.in_(to_langs))
     q_all = q_joins \
         .filter(func.lower(Dictionary.from_roman).contains(text.lower()))
-    q_main = q_all.order_by(
-        Dictionary.from_length,
-        Dictionary.part_of_speech_id, # PartOfSpeech.text,
-        Dictionary.from_word,
-        Dictionary.to_word,
-#       FromLanguage.code,
-#       ToLanguage.code,
-    ).limit(limit)
+    q_main = q_all \
+        .join(PartOfSpeech, Dictionary.part_of_speech_id == PartOfSpeech.id) \
+        .order_by(
+            Dictionary.from_length,
+            PartOfSpeech.text,
+            Dictionary.from_word,
+            Dictionary.to_word,
+       #    FromLanguage.code,
+       #    ToLanguage.code,
+       ).limit(limit)
 
     # Determine column widths
     meta_tpl = '%%-0%ds\t%%0%ds:%%-0%ds\t%%0%ds:%%s'
 
     q_lengths = q_main.from_self() \
+        .join(PartOfSpeech, Dictionary.part_of_speech_id == PartOfSpeech.id) \
         .with_entities(
             func.max(PartOfSpeech.length),
             func.max(FromLanguage.length),
