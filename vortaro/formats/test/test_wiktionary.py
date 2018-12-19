b44d4e94069e00827e95f4bf46fb4628a9730dbc
tlevine <tlevine@noemail.net>
2018-12-19 02:38:57 +0000

    test
    FossilOrigin-Name: b87029002de0db5dd0c8aa815e62f957c6a339df

---
+++ vortaro/formats/test/test_wiktionary.py
@@ -0,0 +1,13 @@
+import pytest
+from ..wiktionary import _translations
+
+cases = (
+    ('ö', '*danska: {{ö+|da|stå}}\n', (('da', 'stå'),)),
+    ('ö', '*franska: {{ö+|fr|se tenir}}\n', (('fr', 'être debout'),)),
+    ('ö', '*franska: {{ö+|fr|se tenir}}, {{ö+|fr|être debout}}\n',
+        (('fr', 'se tenir'), ('fr', 'être debout'))),
+)
+@pytest.mark.parametrize('letter, wikiline, expected', cases)
+def test_translations(letter, wikiline, expected):
+    observed = _translations(letter).parseString(wikiline)
+    assert observed == expected
